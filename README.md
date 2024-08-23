e549915b7cde87de8aac5f9806bed4d1cf4233de
lglof <l.glofcheskie@live.com>
2024-08-23 19:31:28 -0300

    better hall sensor, code works as it should, need better magnet

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 Aiming to make some sort of a speedometer for my bike, ideally for indoor use w/ a trainer. I'd like to have an idea of how fast I'm going and how far I go in one session. 
 
 Code and circuit based on : https://maker.pro/arduino/tutorial/how-to-use-a-hall-effect-sensor-with-arduino
+https://maker.pro/arduino/tutorial/how-to-use-a-hall-effect-sensor-with-arduino
+
 
 using PlatformIO and an arduino Uno for now.
+
+Oh and the specific hall sensor is: https://canada.newark.com/honeywell/ss441r/hall-effect-sensor-uni-polar-to92/dp/50R8558
+
+
+
+[x] ok so something's up w/ the actual detection on the sensor?
+[x] sensor seems perhaps not very sensitive (is this cause I only have SHIT magnets)
+  [x] https://www.farnell.com/datasheets/3953210.pdf based on data sheet I have 441Rs which are listed as high sensitivity
+  [] better magnet needed
+[] is code slow or is sensitivity low
--- src/main.cpp
+++ src/main.cpp
@@ -3,32 +3,35 @@
 volatile byte revolutions;
 unsigned int rpm;
 unsigned long timeold;
+const byte interruptPin = 2;
 
-int myFunction(int, int);
 void magnet_detect();
 
 void setup() {
   Serial.begin(9600);
   Serial.print("Hello World!");
-  attachInterrupt(0, magnet_detect, RISING);
+  // pinMode(2, INPUT);
+
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), magnet_detect, RISING);
   revolutions = 0;
   rpm = 0;
   timeold = 0;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.print(revolutions);
+  Serial.println(revolutions);
+
   if (revolutions >= 20) { 
     rpm = 30*1000/(millis() - timeold)*revolutions;
     timeold = millis();
     revolutions = 0;
-    Serial.println(rpm,DEC);
+    Serial.println("rpm");
+    Serial.println(rpm, DEC);
   } 
   delay(1000);
 }
 
 void magnet_detect() {
   revolutions++;
-  Serial.println("detect");
 }
