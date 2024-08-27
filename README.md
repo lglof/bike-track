53ad74de61fae4c03e8612e00f2e0e3fc9ba8853
lglof <l.glofcheskie@live.com>
2024-08-26 21:14:36 -0300

    total dist travelled

--- README.md
+++ README.md
@@ -13,5 +13,21 @@ Oh and the specific hall sensor is: https://canada.newark.com/honeywell/ss441r/h
 [x] ok so something's up w/ the actual detection on the sensor?
 [x] sensor seems perhaps not very sensitive (is this cause I only have SHIT magnets)
   [x] https://www.farnell.com/datasheets/3953210.pdf based on data sheet I have 441Rs which are listed as high sensitivity
-  [] better magnet needed
+  [] better magnet needed - ordered
 [] is code slow or is sensitivity low
+
+
+information I'd like to be able to get out of this
+- during a bike
+  [] global average speed
+    - this can be calculated via averaging the local averages 
+  [] local average speed (over a period of time? or over a number of rotations)
+- after
+  [x] total distance covered
+    - this is likely just rolling sum 
+
+[] I'd like to dump those into a file that I can grab after at a minimum
+[] ideally we'd be able to upload those to an accessible location after the bike instead
+
+[] all of these imply that there's gotta be a way to start / stop tracking
+[] do we need a total time biked? I'd like one if running a consistent timer isn't shit on the controller
--- src/main.cpp
+++ src/main.cpp
@@ -4,34 +4,43 @@ volatile byte revolutions;
 unsigned int rpm;
 unsigned long timeold;
 const byte interruptPin = 2;
+float wheel_diameter = 0.72; // this is in m
+float wheel_circumference = PI * wheel_diameter;
+float total_dist;
 
 void magnet_detect();
 
 void setup() {
   Serial.begin(9600);
-  Serial.print("Hello World!");
-  // pinMode(2, INPUT);
+  Serial.println("Hello World!");
 
   pinMode(interruptPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(interruptPin), magnet_detect, RISING);
   revolutions = 0;
   rpm = 0;
   timeold = 0;
+  total_dist = 0;
+
+  Serial.println("variable check");
+  Serial.println(wheel_circumference);
+  Serial.println(wheel_diameter);
 }
 
 void loop() {
-  Serial.println(revolutions);
-
-  if (revolutions >= 20) { 
+  if (revolutions >= 5) { 
     rpm = 30*1000/(millis() - timeold)*revolutions;
     timeold = millis();
     revolutions = 0;
     Serial.println("rpm");
     Serial.println(rpm, DEC);
+    Serial.println("total dist");
+    Serial.println(total_dist);
   } 
   delay(1000);
 }
 
 void magnet_detect() {
+  Serial.println("triggered");
   revolutions++;
+  total_dist += wheel_circumference;
 }
